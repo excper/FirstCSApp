b57008cd0887bd5b111845dcf240ef0316080850
Keke Tong <excper@hotmail.com>
2020-06-14 07:02:14 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Player
+{
+    string name;
+    Boolean sex;
+
+    int hp;
+    int attack;
+    int defend;
+
+    public static uint CountOfPlayer;
+    public Player(string n)
+    {
+        name = n;
+        CountOfPlayer++;
+    }
+
+    public void GetInfomation()
+    {
+        Console.WriteLine(name); 
+    }
+}
+
+public class test
+{
+    static void Main()
+    {
+        Player p1 = new Player("LingQi");
+        p1.GetInfomation();
+        Player p2 = new Player("WangZhi");
+        Console.WriteLine(Player.CountOfPlayer);
+    }
+}
